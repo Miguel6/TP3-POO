54017181b0881d828f2821275ee9e8228e8a99cf
Miguel Angel Amengual <amengualma6@gmail.com>
2017-10-01 12:15:42 -0300

    Se agregan Getters&Setters y se implementa el metodo Distancia de UNIDAD
    Se agregan los Getters&Setters, y se implementa el metodo distanciaHasta(Unidad) en la clase abstracta.

--- Desarrollo/src/unidades/Unidad.java
+++ Desarrollo/src/unidades/Unidad.java
@@ -3,10 +3,39 @@ package unidades;
 public abstract class Unidad {
 
 	protected int salud;
-	protected int distanciaDeAtaque;
-	public abstract void atacarA(Unidad otraUnidad);
-	
+	protected int energia;
+	protected int rangoInfAtqADistancia, rangoSupAtqADistancia;
+	protected int daño;
+	protected int posX;
+	protected int posY;
+
+	// Metodos Abstractos
+	protected abstract void atacarA(Unidad otraUnidad);
+
+	// Metodos Implementados
+	protected double distanciaHasta(Unidad otraUnidad) {
+		double difEX=Math.pow(otraUnidad.getPosX()-this.posX,2);
+		double difEY=Math.pow(otraUnidad.getPosY()-this.posY,2);
+		return Math.sqrt(difEX+difEY);		
+	}
+
 	public int getSalud() {
-		return salud;
+		return this.salud;
+	}
+
+	public int getDaño() {
+		return this.daño;
+	}
+
+	public int getEnergia() {
+		return this.energia;
+	}
+
+	public int getPosX() {
+		return this.posX;
+	}
+
+	public int getPosY() {
+		return this.posY;
 	}
 }
