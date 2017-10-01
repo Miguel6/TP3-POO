f684cd311db9d98059c2875ffdd88199d7428064
Miguel Angel Amengual <amengualma6@gmail.com>
2017-10-01 12:38:59 -0300

    Se crea e implementa la clase Lancero

---
+++ Desarrollo/src/unidades/Lancero.java
@@ -0,0 +1,24 @@
+package unidades;
+
+public class Lancero extends Unidad {
+
+	public Lancero(final int posX, final int posY) {
+		this.salud = 150;
+		this.daño = 25;
+		this.rangoInfAtqADist = 1;
+		this.rangoSupAtqADist = 3;
+		this.posX = posX;
+		this.posY = posY;
+
+	}
+
+	@Override
+	public void atacarA(Unidad otraUnidad) {
+		double distHastaUnid = distanciaHasta(otraUnidad);
+		if (distHastaUnid >= this.rangoInfAtqADist && distHastaUnid <= rangoSupAtqADist && otraUnidad.salud > 0) {
+			otraUnidad.esAtacado(daño);
+		}
+
+	}
+
+}
